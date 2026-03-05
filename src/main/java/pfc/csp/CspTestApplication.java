35e8e658a6ef7d861941044d0e97779a513b8365
pafciu <2478856+pafciu@users.noreply.github.com>
2026-03-05 15:04:50 +0100

    placeholder Spring Boot project

---
+++ src/main/java/pfc/csp/CspTestApplication.java
@@ -0,0 +1,13 @@
+package pfc.csp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CspTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CspTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pfc/csp/CspTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pfc.csp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
